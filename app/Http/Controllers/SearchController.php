1495480ea11d3eda34ae528524ea09b3d3fe5ca9
KKawaGS <kamil.kawa@gate-software.com>
2021-10-04 22:10:37 +0200

    Add SearchRequestParser for parsing input string, basic validation for input in controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\HelperClass\SearchRequestParser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
@@ -13,6 +14,9 @@ class SearchController extends Controller
         $validatedData = $request->validate([
             'search' => ['required', 'max:255', 'regex:/^[\w ]{3,}[|][\w ]+[<>]\s*\d+\s*$/']
         ]);
+
+        $searchRequestParser = new SearchRequestParser($validatedData['search']);
+        $searchParams = $searchRequestParser->getData();
 
         return view('index');
     }
